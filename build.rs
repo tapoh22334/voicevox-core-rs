7b1fd4fd2bc744bf351ca8ad0cbeea881df0f24f
iwase22334 <iwase22334@gmail.com>
2023-03-31 23:33:22 +0900

    remove unused deps

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 fn main(){
     println!("cargo:rustc-link-search=native=voicevox_core");
     println!("cargo:rustc-link-lib=voicevox_core");
-    //println!("cargo:rustc-link-lib=onnxruntime");
-    //println!("cargo:rustc-link-lib=onnxruntime_providers_shared");
 }
